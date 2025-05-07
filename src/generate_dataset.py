7e9bce84ff6cddf935a39f8cbc2a8db967e29853
vint <vint@berkeley.edu>
2025-05-07 01:29:40 +0000

    dataset generation done

--- src/generate_dataset.py
+++ src/generate_dataset.py
@@ -36,7 +36,7 @@ def render_cad_data(cad_data, rolls, elevations):
         images.append(img)
     return images
 
-def main(dataset_dir, train_size, val_size, output_dir, images_per_cad=8, save_every=10):
+def main(dataset_dir, train_size, val_size, output_dir, images_per_cad=8, save_every=1000):
     """
     given dataset directory, size of training and validation split
     saves renders to output dir
@@ -79,9 +79,9 @@ def main(dataset_dir, train_size, val_size, output_dir, images_per_cad=8, save_e
 
 if __name__=="__main__":
     dataset_dir = "datasets/cad-recode-v1.5"
-    output_dir = "datasets/cad-recode-render"
-    train_size = 100
-    val_size = 20
+    output_dir = "datasets/cad-recode-render-v1"
+    train_size = 20000
+    val_size = 1000
     images_per_cad = 8
 
     main(
